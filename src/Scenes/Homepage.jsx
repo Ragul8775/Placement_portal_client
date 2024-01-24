45e96dab129eb5afed8fb97758224f29ca53fd1e
Kishore2713 <raghupathykiran@gmail.com>
2024-01-24 11:57:49 +0530

    nav

--- src/Scenes/Homepage.jsx
+++ src/Scenes/Homepage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Profile from '../widgets/Profile'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <Navbar/>
+      <Profile/>
+    </div>
   )
 }
 
